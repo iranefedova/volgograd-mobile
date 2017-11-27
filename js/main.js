2172641016add2dd76aaefde6e22d0f5abc21b1d
Ira Nefedova <iraonegina@yandex.ru>
2017-11-27 13:59:55 +0300

    about budget with image map

--- js/main.js
+++ js/main.js
@@ -19,6 +19,34 @@ $(function(){
     $('.toggle-item').on('click',function(){
         $(this).next().slideToggle(300);
     });
+    
+    $('#Map area:first').on('click', function(){
+        $('#incomes-block').slideDown(300);
+        $('#costs-block').slideUp(300);
+    });
+    
+    $('#Map area:last').on('click', function(){
+        $('#costs-block').slideDown(300);
+        $('#incomes-block').slideUp(300);
+    });
+    
+    $('.costs-1').on('click', function(){
+        $('#costs-2').slideUp(300);
+        $('#costs-3').slideUp(300);
+        $('#costs-1').slideDown(300);
+    });
+    
+    $('.costs-2').on('click', function(){
+        $('#costs-1').slideUp(300);
+        $('#costs-3').slideUp(300);
+        $('#costs-2').slideDown(300);
+    });
+    
+    $('.costs-3').on('click', function(){
+        $('#costs-2').slideUp(300);
+        $('#costs-1').slideUp(300);
+        $('#costs-3').slideDown(300);
+    });
 
 //    $( "#datepicker, .datepicker" ).datepicker( $.datepicker.regional[ "ru" ] );
 
