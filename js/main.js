a4c8f66a078d446e5635046117fd1054c27416db
Ira Nefedova <iraonegina@yandex.ru>
2017-11-27 15:08:56 +0300

    tax benefits

--- js/main.js
+++ js/main.js
@@ -1,56 +1,77 @@
-$(function(){
+$(function () {
 
-    $('.burger').on('click',function(){
+    $('.burger').on('click', function () {
         $(this).toggleClass('open');
         $(this).next().slideToggle(300);
     });
 
-    $('.btn').on('click',function(){
+    $('.btn').on('click', function () {
         $(this).toggleClass('open');
         $(this).next().slideToggle(300);
     });
 
 
-    $('a.root-item').on('click',function(e){
+    $('a.root-item').on('click', function (e) {
         e.preventDefault();
         $(this).next().slideToggle(300);
     });
 
-    $('.toggle-item').on('click',function(){
+    $('.toggle-item').on('click', function () {
         $(this).next().slideToggle(300);
     });
-    
-    $('#Map area:first').on('click', function(){
+
+    $('#Map area:first').on('click', function () {
         $('#incomes-block').slideDown(300);
         $('#costs-block').slideUp(300);
     });
-    
-    $('#Map area:last').on('click', function(){
+
+    $('#Map area:last').on('click', function () {
         $('#costs-block').slideDown(300);
         $('#incomes-block').slideUp(300);
     });
-    
-    $('.costs-1').on('click', function(){
+
+    $('.costs-1').on('click', function () {
         $('#costs-2').slideUp(300);
         $('#costs-3').slideUp(300);
         $('#costs-1').slideDown(300);
     });
-    
-    $('.costs-2').on('click', function(){
+
+    $('.costs-2').on('click', function () {
         $('#costs-1').slideUp(300);
         $('#costs-3').slideUp(300);
         $('#costs-2').slideDown(300);
     });
-    
-    $('.costs-3').on('click', function(){
+
+    $('.costs-3').on('click', function () {
         $('#costs-2').slideUp(300);
         $('#costs-1').slideUp(300);
         $('#costs-3').slideDown(300);
     });
 
-//    $( "#datepicker, .datepicker" ).datepicker( $.datepicker.regional[ "ru" ] );
+    //    $( "#datepicker, .datepicker" ).datepicker( $.datepicker.regional[ "ru" ] );
 
 
     $('select').styler();
+    
+    $("#main-factors").tableHeadFixer({"head" : true, "left" : 1}); 
+
+    $('.spoiler').click(function () {
+        var $parent = $(this).parent("td").parent(".first");
+        var $sp = $(this);
+        $(this).toggleClass("fa-angle-right").toggleClass("fa-angle-down");
+
+        if ($parent.length < 1) {
+            $parent = $(this).parent("td").parent(".second");
+            $parent.nextUntil(".second, .first").slideToggle(200);
+            return;
+        }
+
+        $parent.nextUntil(".first", ".fourth").hide(100);
+        $parent.nextUntil(".first", ".third").hide(100);
+        $parent.nextUntil(".first", ".second").slideToggle(200);
+        $parent.nextUntil(".first", ".second").each(function () {
+            $(this).children("td:first").children("i").removeClass("fa-angle-down").addClass("fa-angle-right");
+        });
+    });
 
 });
